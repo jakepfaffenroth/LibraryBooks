99f207ed13d7c256977e542fbdec008a630e1f41
jakepfaffenroth <jakepfaffenroth@gmail.com>
2020-04-29 06:53:14 -0700

    Fixed delete buttons and read buttons

--- script.js
+++ script.js
@@ -3,35 +3,24 @@ const libraryArea = document.querySelector('#libraryArea');
 let bookCardsAll = document.querySelectorAll('#bookCard');
 let titleTxtBox = document.querySelector('#titleInput');
 let authorTxtBox = document.querySelector('#authorInput');
-let readCheckBox = document.querySelector('#readInput')
+let readCheckBox = document.querySelector('#readInput');
 const confirmationMsg = document.querySelector('#entryConfirmation');
+let allDeleteBtns = [];
+let allReadBtns = [];
 
 let myLibrary = [];
+
 myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', true));
 myLibrary.push(new Book('1984', 'George Orwell', true));
 myLibrary.push(new Book('Meditations', 'Marcus Aurelius', false));
+
 render();
 
 function Book(title, author, read) {
     this.title = title;
     this.author = author;
     this.read = read;
-    // this.getInfo = function () {
-    //     return `${this.title}
-    //     ${\n}
-    //     ${this.author}`;
-    // };
 }
-
-// Book.prototype.add = function (x, y) {
-//     var book = { title: x, author: y };
-//     myLibrary.push(book);
-//     console.log(`Added ${x} by ${y} to myLibrary`);
-// };
-
-// Book.prototype.getInfo = function () {
-//     return `${this.title}, by ${this.author}`;
-// };
 
 // TODO: Create popup form for book entry instead of the text fields
 function addBookToLibrary() {
@@ -64,39 +53,50 @@ function render() {
         var bookCard = document.createElement('p');
         var bookTitle = document.createElement('p');
         var bookAuthor = document.createElement('p');
-        var readStatus = document.createElement('div')
+        var readStatus = document.createElement('div');
         var deleteBtn = document.createElement('button');
-        // var bookInfo = document.createElement('div');
+        var readBtn = document.createElement('button');
 
         bookCard.classList.add('bookCard');
         // bookInfo.classList = 'bookInfo';
-        bookTitle.classList.add('bookTitle')
-        bookAuthor.classList.add('bookAuthor')
-        readStatus.classList.add('bookRead', 'notRead')
-        deleteBtn.classList.add('deleteBtn')
+        bookTitle.classList.add('bookTitle');
+        bookAuthor.classList.add('bookAuthor');
+        readStatus.classList.add('bookRead', 'notRead');
+        deleteBtn.classList.add('deleteBtn');
+        readBtn.classList.add('readBtn');
 
         bookTitle.textContent = book.title;
         bookAuthor.textContent = book.author;
         deleteBtn.textContent = 'Remove';
         deleteBtn.id = book.title;
+        readBtn.id = book.title;
 
         if (book.read == true) {
-            readStatus.classList.remove('notRead')
-            readStatus.classList.add('read')
+            readStatus.classList.remove('notRead');
+            readStatus.classList.add('read');
         }
 
         // bookCard.appendChild(bookInfo);
         bookCard.appendChild(bookTitle);
         bookCard.appendChild(bookAuthor);
         bookCard.appendChild(deleteBtn);
-        bookCard.appendChild(readStatus)
+        bookCard.appendChild(readBtn);
+        bookCard.appendChild(readStatus);
         libraryArea.appendChild(bookCard);
         // console.table(myLibrary);
     });
     titleTxtBox.value = '';
     authorTxtBox.value = '';
-    readCheckBox.checked=false
+    readCheckBox.checked = false;
+
+    allDeleteBtns = document.querySelectorAll('.deleteBtn');
+    allReadButtons = document.querySelectorAll('.readBtn');
     activateBtns();
+    Object.prototype.readToggle = function (book) {
+        console.log(book.read);
+        book.read === true ? book.read=false : book.read=true;
+        console.log(book.read);
+    };
 }
 
 function deleteBook(index) {
@@ -106,12 +106,24 @@ function deleteBook(index) {
 }
 
 submitBtn.addEventListener('click', addBookToLibrary);
-
+// TODO: On clicking read toggle, having trouble retrieving the book object in order to change the read status.
 function activateBtns() {
-    let allDeleteBtns = document.querySelectorAll('.deleteBtn');
-    allDeleteBtns.forEach((btn) => {
-        btn.addEventListener('click', function (e) {
-            let index = myLibrary.findIndex((book) => book.title == e.target.dataset.title);
+    allReadButtons.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            const index = myLibrary.findIndex((book) => book.title === btn.id);
+            let book = myLibrary[index]
+            console.log(book.title);
+            book.readToggle(book);
+            console.table(myLibrary);
+            render();
+        });
+    });
+
+    allDeleteBtns.forEach(function (btn) {
+        btn.addEventListener('click', (e) => {
+            const index = myLibrary.findIndex((book) => book.title === btn.id);
+            console.log('obj title: ' + myLibrary[index].title);
+            console.log(index);
             deleteBook(index);
         });
     });
